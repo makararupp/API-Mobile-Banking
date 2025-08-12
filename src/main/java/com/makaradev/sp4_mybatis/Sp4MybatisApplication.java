20d1ea734381b1a8e541af23b4763379264f25d9
makaraDevIt <makarasam68@gmail.com>
2025-08-12 16:11:30 +0700

    configure_appliction.properties_Oracle

---
+++ src/main/java/com/makaradev/sp4_mybatis/Sp4MybatisApplication.java
@@ -0,0 +1,13 @@
+package com.makaradev.sp4_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp4MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp4MybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makaradev/sp4_mybatis/Sp4MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaradev.sp4_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp4MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
